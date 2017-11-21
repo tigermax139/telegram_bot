ba54c049723d3162be155caadf07caf25aae3a89
tigermax139 <tigermax139@gmail.com>
2017-11-22 00:53:24 +0200

    added new imd, before and after load img, bot send message

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = '508235796:AAEGb0P1Y0NpegyfM6OfkQE07s8_QCmRus8';
+const fs = require('fs');
+const  _ = require('lodash');
 
 const bot = new TelegramBot(TOKEN, {
     polling: true
@@ -14,14 +16,18 @@ const KB = {
 
 const PicSrcs = {
     [KB.cat]: [
-        'car.png',
-        'car1.png',
-        'car2.jpg'
-    ],
-    [KB.car]: [
         'cat.png',
         'cat1.jpg',
-        'cat2.jpg'
+        'cat2.jpg',
+        'cat3.jpg',
+        'cat4.jpg'
+    ],
+    [KB.car]: [
+        'car.png',
+        'car1.png',
+        'car2.jpg',
+        'car3.jpg',
+        'car4.jpg'
     ]
 }
 
@@ -72,4 +78,16 @@ function sendGreeting(msg, sayHello = true) {
 
 function sendPictureByName(chatId, picName) {
     const srcs = PicSrcs[picName];
+
+    const src = srcs[_.random(0, srcs.length - 1)];
+
+    bot.sendMessage(chatId, `Loading ....`);
+
+    fs.readFile(`${__dirname}/img/${src}`, (error, picture) => {
+        if (error) throw new Error(error);
+
+        bot.sendPhoto(chatId, picture).then( () => {
+            bot.sendMessage(chatId, `Completed! :)`);
+        });
+    })
 }
