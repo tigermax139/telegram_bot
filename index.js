ef2e2c276a185c6574afab4acc993495a9b3b49a
tigermax139 <tigermax139@gmail.com>
2017-11-22 16:17:36 +0200

    added img timeTable

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const request = require('request');
 const  _ = require('lodash');
 
+//const VNAU = 'http://81.30.162.30/timeTable/group?filial=0&faculty=5&course=3&group=1873&date1=06.09.2015&date2=01.11.2015&r11=5&&TimeTableForm[filial]=0&TimeTableForm[faculty]=5&TimeTableForm[course]=3&TimeTableForm[group]=1873&TimeTableForm[date1]=22.11.2017&TimeTableForm[date2]=09.01.2018'
+
 const bot = new TelegramBot(TOKEN, {
     polling: true
 });
@@ -12,7 +14,8 @@ const KB = {
     picture: 'picture',
     cat: 'cat',
     car: 'car',
-    back: 'back'
+    back: 'back',
+    table: 'TimeTable'
 };
 
 const PicSrcs = {
@@ -49,7 +52,10 @@ bot.on('message', msg => {
             break;
         case KB.cat:
         case KB.car:
-            sendPictureByName(msg.chat.id, msg.text)
+            sendPictureByName(msg.chat.id, msg.text);
+            break;
+        case KB.table:
+            sendTableScreen(msg.chat.id, msg.date);
             break;
     }
 } );
@@ -98,7 +104,8 @@ function sendGreeting(msg, sayHello = true) {
     bot.sendMessage(msg.chat.id, text, {
         reply_markup: {
             keyboard: [
-                [KB.currency, KB.picture ]
+                [KB.currency, KB.picture ],
+                [KB.table]
             ]
         }
     });
@@ -136,5 +143,32 @@ function sendCurrencyScreen(chatId, ) {
             ]
 
         }
+    })
+}
+
+/****teamTable****/
+function sendTableScreen(chatId, date) {
+    timeConverter(date, chatId);
+   // bot.sendMessage(chatId, `test ${time}`);
+}
+function timeConverter(UNIX_timestamp, chatId) {
+    const a = new Date(UNIX_timestamp * 1000);
+    const months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+    const year = a.getFullYear();
+    const month = months[a.getMonth()];
+    const date = a.getDate();
+    const hour = a.getHours();
+    const min = a.getMinutes();
+    const sec = a.getSeconds();
+    const time = date + '-' + month + '-' + year;
+
+    bot.sendMessage(chatId, `test ${time}`);
+
+    fs.readFile(`${__dirname}/img/table/${time}.png`, (error, picture) => {
+        if (error) throw new Error(error);
+
+        bot.sendPhoto(chatId, picture).then( () => {
+            bot.sendMessage(chatId, `Completed! :)`);
+        });
     })
 }
